99d3014bc3ae0562dd9565e9c002ae0bb87bc33e
mholsten <michel@holsten.name>
2022-01-05 11:54:25 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'split-it-6',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
