5c75b9200e47245c61ad35e631511cc53cccb81e
mholsten <michel@holsten.name>
2023-08-25 14:13:02 +0200

    added SQLite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 /// <reference types="vitest" />
-import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+  },
+  build: {
+    target: 'esnext',
   },
 })
