bf3aaacc43726bc502f0a1ea393e35c61c0b9e3c
mholsten <michel@holsten.name>
2025-08-19 21:46:16 +0200

    [FR-002] - updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-/// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'esnext',
-  },
 })
