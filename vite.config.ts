ae0ac6128e8f05bd922334ba36b1d89aff57a3c0
mholsten <michel@holsten.name>
2025-08-19 21:39:47 +0200

    [FR-002] - removed testing stuff

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  },
   build: {
     target: 'esnext',
   },
