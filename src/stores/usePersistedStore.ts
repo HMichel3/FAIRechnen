5a92749fc1df5b5870c257f6e978b2764b19cfe8
mholsten <michel@holsten.name>
2026-03-15 20:11:23 +0100

    fixed data storing

--- src/stores/usePersistedStore.ts
+++ src/stores/usePersistedStore.ts
@@ -1,4 +1,5 @@
 import { Drivers, Storage } from '@ionic/storage'
+// automatically uses cordova-sqlite-storage if installed via package.json
 import CordovaSQLiteDriver from 'localforage-cordovasqlitedriver'
 import { create, StateCreator } from 'zustand'
 import { createJSONStorage, persist, StateStorage } from 'zustand/middleware'
@@ -11,12 +12,36 @@ import { createInfoSlideSlice, InfoSlideSlice } from './slices/createInfoSlideSl
 import { createMemberSlice, MemberSlice } from './slices/createMemberSlice'
 import { createPurchaseSlice, PurchaseSlice } from './slices/createPurchaseSlice'
 
+const STORAGE_NAME = '__db'
+const STORAGE_KEY = 'store-storage'
+
 const ionicStorage = new Storage({
-  name: '__db',
+  name: STORAGE_NAME,
   driverOrder: [CordovaSQLiteDriver._driver, Drivers.IndexedDB, Drivers.LocalStorage],
 })
 await ionicStorage.defineDriver(CordovaSQLiteDriver)
 await ionicStorage.create()
+
+async function migrateIndexedDBtoSQLite() {
+  const activeDriver = ionicStorage.driver
+  if (activeDriver === CordovaSQLiteDriver._driver) {
+    const sqliteData = await ionicStorage.get(STORAGE_KEY)
+    if (!sqliteData) {
+      const fallbackStorage = new Storage({
+        name: STORAGE_NAME,
+        driverOrder: [Drivers.IndexedDB],
+      })
+      await fallbackStorage.create()
+      const oldIndexedDbData = await fallbackStorage.get(STORAGE_KEY)
+      if (oldIndexedDbData) {
+        await ionicStorage.set(STORAGE_KEY, oldIndexedDbData)
+        await fallbackStorage.remove(STORAGE_KEY)
+      }
+    }
+  }
+}
+
+await migrateIndexedDBtoSQLite() // needed because of bug on previous app version, where the data was only stored in indexedDB
 
 const storage: StateStorage = {
   getItem: async key => {
@@ -59,7 +84,7 @@ export const usePersistedStore = create<PersistedState>()(
       _hasHydrated: false,
     })),
     {
-      name: 'store-storage',
+      name: STORAGE_KEY,
       storage: createJSONStorage(() => storage),
       partialize: state => ({
         groups: state.groups,
